80a2e43f0dba2ace6a46c333419f1b32b32bf042
TijnBerns <tijn_berns@hotmail.com>
2021-11-22 10:18:23 +0100

    Changed implementation to an object oriented style

--- timingReceive.py
+++ timingReceive.py
@@ -1,13 +1,34 @@
 from scapy.all import *
+from abc import ABC, abstractmethod
 import datetime
 
-timings=[]
 
-def handle_packet(packet):
-    timings.append(datetime.datetime.now())
+class Receiver(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.timings = []
 
-sniff(filter='icmp[icmptype] == icmp-echo', prn=handle_packet, store=0, timeout=10)
+    def __handle_packet(self, packet) -> None:
+        self.timings.append(datetime.datetime.now())
 
-for i in range(len(timings) - 1):
-    time_diff = timings[i+1] - timings[i]
-    print(time_diff.total_seconds() * 1000 / 100)
+    def sniff_packet(self) -> None:
+        sniff(filter='icmp[icmptype] == icmp-echo',
+              prn=self.__handle_packet, store=0, timeout=10)
+
+    @abstractmethod
+    def decrypt_message(self) -> str:
+        pass
+
+
+class SimpleReceiver(Receiver):
+    def decrypt_message(self) -> str:
+        for i in range(len(self.timings) - 1):
+            time_diff = self.timings[i+1] - self.timings[i]
+            print(time_diff.total_seconds() * 1000 / 100)
+        return ""
+
+
+if __name__ == "__main__":
+    sr = SimpleReceiver()
+    sr.sniff_packet
+    sr.decrypt_message
--- timingSend.py
+++ timingSend.py
@@ -1,24 +1,63 @@
+from scapy.all import *
+from abc import ABC, abstractmethod
 import numpy as np
 import time
-from scapy.all import *
 import os
 
-hidden_message_str = "TestString"
-hidden_message_base_8 = [oct(ord(c)) for c in hidden_message_str]
-hidden_message_intervals_str = np.array([list(s[2:]) for s in hidden_message_base_8]).flatten()
-hidden_message_intervals_int = [int(c) for c in hidden_message_intervals_str]
-print(hidden_message_intervals_int)
 
-# packet =  Ether() / IP(dst='www.google.com') / ICMP() / "XXXXXXXXXXX"
-# sendp(packet, iface=interface)
-# Warmup
-os.system("ping -c 1 www.google.com")
-os.system("ping -c 1 www.google.com")
-os.system("ping -c 1 www.google.com")
-os.system("ping -c 1 www.google.com")
-os.system("ping -c 1 www.google.com")
+class MessageConverter(ABC):
+    @abstractmethod
+    def message_to_intervals(self, message: str) -> List[int]:
+        pass
 
-for interval in hidden_message_intervals_int:
-    time.sleep((interval*100) / 1000)
-    os.system("ping -c 1 www.google.com")
 
+class SimpleMessageConverter(MessageConverter):
+    def message_to_intervals(self, message: str) -> List[int]:
+        """Converts a message into a set of time intervals. 
+        Conversion is done by taking the base 8 ascii value of each character in the message.
+
+        Args:
+            message (String): The message that is converted to intervals
+
+        Returns:
+            List: List of integers representing the intervals
+        """
+        message_base_8 = [oct(ord(c)) for c in message]
+        message_intervals_str = np.array(
+            [list(s[2:]) for s in message_base_8]).flatten()
+        hidden_message_intervals_int = [int(c) for c in message_intervals_str]
+        return hidden_message_intervals_int
+
+
+class StreamMessageConverter(MessageConverter):
+    def message_to_intervals(self, message: str) -> List[int]:
+        raise NotImplementedError
+
+
+class Sender(object):
+    def __init__(self, message_converter: MessageConverter) -> None:
+        super().__init__()
+        self.message_converter = message_converter
+
+    def send_message(self, message: str) -> None:
+        """Sends ping messages to receiver at corresponding time intervals
+
+        Args:
+            message (String): The plaintext message that is send to the receiver
+            receiver (String): The IP-address of the receiver
+        """
+        # Warmup
+        for _ in range(5):
+            os.system("ping -c 1 www.google.com")
+
+        # Send pings at message intervals
+        for interval in self.message_converter.message_to_intervals(message):
+            time.sleep((interval*100) / 1000)
+            os.system("ping -c 1 www.google.com")
+
+
+if __name__ == "__main__":
+    smc = SimpleMessageConverter()
+    simple_sender = Sender(smc)
+    message = "AttackAtDawn"
+    simple_sender.send_message(message)
