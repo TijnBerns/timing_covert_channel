1ed615fcb0a5a2ac761e1c51c24a3b828ae34aa5
MahieB <51122015+MahieB@users.noreply.github.com>
2021-11-28 22:16:44 +0100

    Improved performance
    Improved performance,
    while sending: replaced the os.system() call with Scapy functions for increased efficiency.
    while receiving: added additional parameters to sniff() to reduce its workload, and replaced datetime.now() with time() which is more efficient to compute intervals.
    Consequently, intervals can reliably reduced from 50ms to 5ms.

--- timingReceive.py
+++ timingReceive.py
@@ -1,7 +1,6 @@
 from scapy.all import *
 from abc import ABC, abstractmethod
-import datetime
-
+import time
 
 class Receiver(ABC):
     def __init__(self) -> None:
@@ -9,11 +8,11 @@ class Receiver(ABC):
         self.timings = []
 
     def __handle_packet(self, packet) -> None:
-        self.timings.append(datetime.datetime.now())
+        self.timings.append(time.time())
 
     def sniff_packet(self) -> None:
         sniff(filter='icmp[icmptype] == icmp-echo',
-              prn=self.__handle_packet, store=0, timeout=10)
+              prn=self.__handle_packet, store=False, quiet=True, iface="enp3s0", timeout=5)
 
     @abstractmethod
     def decrypt_message(self) -> str:
@@ -24,11 +23,11 @@ class SimpleReceiver(Receiver):
     def decrypt_message(self) -> str:
         for i in range(len(self.timings) - 1):
             time_diff = self.timings[i+1] - self.timings[i]
-            print(time_diff.total_seconds() * 1000 / 100)
+            print(time_diff * 1000 / 5)
         return ""
 
 
 if __name__ == "__main__":
     sr = SimpleReceiver()
-    sr.sniff_packet
-    sr.decrypt_message
+    sr.sniff_packet()
+    sr.decrypt_message()
--- timingSend.py
+++ timingSend.py
@@ -1,26 +1,23 @@
-from scapy.all import *
+from scapy.all import Ether, IP, ICMP, raw, conf
 from abc import ABC, abstractmethod
+from typing import List
 import numpy as np
 import time
-import os
-
+import IPs
 
 class MessageConverter(ABC):
     @abstractmethod
     def message_to_intervals(self, message: str) -> List[int]:
         pass
 
-
 class SimpleMessageConverter(MessageConverter):
     def message_to_intervals(self, message: str) -> List[int]:
         """Converts a message into a set of time intervals. 
         Conversion is done by taking the base 8 ascii value of each character in the message.
-
         Args:
-            message (String): The message that is converted to intervals
-
+            message (String): The message that is converted to intervals.
         Returns:
-            List: List of integers representing the intervals
+            List: List of integers representing the intervals.
         """
         message_base_8 = [oct(ord(c)) for c in message]
         message_intervals_str = np.array(
@@ -33,31 +30,33 @@ class StreamMessageConverter(MessageConverter):
     def message_to_intervals(self, message: str) -> List[int]:
         raise NotImplementedError
 
-
 class Sender(object):
     def __init__(self, message_converter: MessageConverter) -> None:
         super().__init__()
         self.message_converter = message_converter
 
     def send_message(self, message: str) -> None:
-        """Sends ping messages to receiver at corresponding time intervals
-
+        """Sends ping messages to receiver at corresponding time intervals.
         Args:
-            message (String): The plaintext message that is send to the receiver
-            receiver (String): The IP-address of the receiver
+            message (String): The plaintext message that is send to the receiver.
+            receiver (String): The IP-address of the receiver.
         """
-        # Warmup
-        for _ in range(5):
-            os.system("ping -c 1 www.google.com")
 
-        # Send pings at message intervals
-        for interval in self.message_converter.message_to_intervals(message):
-            time.sleep((interval*100) / 1000)
-            os.system("ping -c 1 www.google.com")
+        # Prepare both the raw ping message and a socket.
+        packet = Ether() / IP(dst=IPs.dst, src=IPs.src) / ICMP()
+        frame = bytearray(raw(packet))
+        s = conf.L2socket()
+        # Send pings at message intervals.
+        intervals = self.message_converter.message_to_intervals(message)
+        for interval in intervals:
+            time.sleep((interval*5) / 1000)
+            s.send(frame)
+        return intervals
 
 
 if __name__ == "__main__":
     smc = SimpleMessageConverter()
     simple_sender = Sender(smc)
     message = "AttackAtDawn"
-    simple_sender.send_message(message)
+    intervals = simple_sender.send_message(message)
+    print(intervals) # Print for debugging.
