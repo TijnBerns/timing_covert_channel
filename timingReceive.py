5ab19a4369307fd3fba028072ccabeb4e29ad3b1
Mathieu Bangma <mbangma@pingpong.cs.ru.nl>
2021-12-13 12:02:19 +0000

    Added sender and receiver which do not use standard interval of 1 second

--- timingReceive.py
+++ timingReceive.py
@@ -2,6 +2,8 @@ from scapy.all import *
 from abc import ABC, abstractmethod
 import time
 import numpy as np
+import IPs
+from args import parser
 
 
 class Receiver(ABC):
@@ -15,7 +17,7 @@ class Receiver(ABC):
         Args:
             timeout (int, optional): Time the network is sniffed (s). Defaults to 15.
         """
-        sniff(filter='icmp[icmptype] == icmp-echo',
+        sniff(filter='icmp[icmptype] == icmp-echo && src host ' + IPs.pub,
               prn=self.__handle_packet, store=False, quiet=True, iface="enp3s0", timeout=timeout)
 
     def get_time_diff_ms(self) -> np.array(float):
@@ -35,7 +37,7 @@ class Receiver(ABC):
         self.timings.append(time.time())
 
     @abstractmethod
-    def decrypt_message(self, base=20, delay=30) -> str:
+    def decrypt_message(self, base, delay) -> str:
         pass
 
 
@@ -43,15 +45,51 @@ class SimpleReceiver(Receiver):
     def __init__(self) -> None:
         super().__init__()
 
-    def decrypt_message(self, base: int = 7, delay: int = 50) -> str:
+    def decrypt_message(self, base: int, delay: int) -> str:
         # Get the intervals between pings in ms
         msg_enc = self.get_time_diff_ms() - 1000
 
         # Ronud intervals based on delay
         msg_enc = delay * np.round(msg_enc / delay)
 
-        # Convert to different base
-        msg_enc = msg_enc / delay + (base // 2)
+        # Convert to numbers in given base
+        if (base % 2 == 1):
+            msg_enc = msg_enc / delay + (base // 2)
+        else:
+            msg_enc -= delay / 2000
+            msg_enc = msg_enc / delay + (base // 2)
+
+        # Clip to valid range
+        msg_enc = np.clip(msg_enc, 0, base-1).astype('int')
+
+        # Convert to array of characters
+        msg_enc_str = msg_enc.astype(str)
+
+        # Compute the max length of a single character in given base
+        max_len = len(np.base_repr(26, base=base))
+
+        # Take slices of msg_enc_str with length of max_len
+        msg_base = ["".join(msg_enc_str[i:max_len+i])
+                    for i in range(0, len(msg_enc_str) - 1, max_len)]
+
+        # Convert to base 10, then to ascii, then to characters
+        msg_dec = "".join([chr(int(msg, base) + 97) for msg in msg_base])
+        return msg_dec
+
+
+class NoIntervalReceiver(Receiver):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def decrypt_message(self, base: int, delay: int) -> str:
+        # Get the intervals between pings in ms
+        msg_enc = self.get_time_diff_ms()
+
+        # Ronud intervals based on delay
+        msg_enc = delay * np.round(msg_enc / delay)
+
+        # Convert to numbers in given base
+        msg_enc = msg_enc / delay - 1
 
         # Clip to valid range
         msg_enc = np.clip(msg_enc, 0, base-1).astype('int')
@@ -72,13 +110,26 @@ class SimpleReceiver(Receiver):
 
 
 if __name__ == "__main__":
-    sr = SimpleReceiver()
-    sr.sniff_packet(60)
-    msg = sr.decrypt_message()
-    intervals = sr.get_time_diff_ms()
+    args = parser()
 
+    # Initialize receiver
+    if args.type == 1:
+        recv = SimpleReceiver()
+    elif args.type == 2: 
+        recv = NoIntervalReceiver()
+    else:
+        raise NotImplementedError
+
+    # Receive pings and decrypt message
+    recv.sniff_packet(args.timeout)
+    msg = recv.decrypt_message(args.base, args.delay)
+    intervals = recv.get_time_diff_ms()
+
+    # Print info
     if len(intervals) > 0:
-        print(f"REVEIVED MESSAGE: {msg}\n")
+        print(f"RECEIVED INTERVALS (ms):\n{intervals}\n")
+
+        print(f"RECEIVED MESSAGE:\n{msg}\n")
 
         print(f"STATISTICS OF RECEIVED PINGS\n" +
               f"Count: {len(intervals)}\n" +
--- timingSend.py
+++ timingSend.py
@@ -1,19 +1,14 @@
 from scapy.all import Ether, IP, ICMP, raw, conf
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import List
 import numpy as np
 import time
 import IPs
+from args import parser
 
 
 class MessageConverter(ABC):
-    @abstractmethod
-    def message_to_intervals(self, message: str) -> List[int]:
-        pass
-
-
-class SimpleMessageConverter(MessageConverter):
-    def message_to_intervals(self, msg: str, base: int, delay: int = 50) -> List[int]:
+    def message_to_intervals(self, msg: str, base: int) -> List[int]:
         """Converts a message into a set of time intervals. 
 
         Args:
@@ -22,28 +17,50 @@ class SimpleMessageConverter(MessageConverter):
             List: List of integers representing the intervals in seconds.
         """
         max_len = len(np.base_repr(26, base=base))
-        
+
         # Convert to lower and remove whitespace
         msg = np.array(list(msg.lower().replace(" ", "")))
-        
+
         # Convert to integers
-        msg_int = msg.view(np.int32) - 97 
-        
+        msg_int = msg.view(np.int32) - 97
+
         # Convert to specified base
-        msg_base = np.array([np.base_repr(c, base=base) for c in msg_int])  
-        
+        msg_base = np.array([np.base_repr(c, base=base) for c in msg_int])
+
         # Prepend 0's to ensure equal lengths
         msg_base = np.array([((max_len - len(c)) * '0') + c for c in msg_base])
-        
-        # Convert strings to time intervals 
-        intervals_str = np.array([list(s) for s in msg_base]).flatten() 
-        intervals_int = intervals_str.astype(np.int)
+
+        # Convert strings to time intervals
+        intervals_str = np.array([list(s) for s in msg_base]).flatten()
+        return intervals_str.astype(int)
+
+
+class SimpleMessageConverter(MessageConverter):
+    def message_to_intervals(self, msg: str, base: int, delay: int) -> List[int]:
+        """Converts a message into a set of time intervals.
+        Adds a standard interval of 1 sec, and centers computed intervals around 1 sec.
+
+        Args:
+            message (String): The message that is converted to intervals.
+        Returns:
+            List: List of integers representing the intervals in seconds.
+        """
+        intervals_int = super().message_to_intervals(msg, base)
         return ((intervals_int - (base // 2)) * delay) / 1000 + 1
 
 
-class StreamMessageConverter(MessageConverter):
-    def message_to_intervals(self, message: str) -> List[int]:
-        raise NotImplementedError
+class NoIntervalMessageConverter(MessageConverter):
+    def message_to_intervals(self, msg: str, base: int, delay: int) -> List[int]:
+        """Converts a message into a set of time intervals.
+        Adds a standard interval of 1 ms * delay (without centering).
+
+        Args:
+            message (String): The message that is converted to intervals.
+        Returns:
+            List: List of integers representing the intervals in seconds.
+        """
+        intervals_int = super().message_to_intervals(msg, base)
+        return (intervals_int + 1) * delay / 1000
 
 
 class Sender(object):
@@ -51,19 +68,19 @@ class Sender(object):
         super().__init__()
         self.message_converter = message_converter
 
-    def send_message(self, message: str, base: int) -> None:
+    def send_message(self, message: str, base: int, delay: int) -> None:
         """Sends ping messages to receiver at corresponding time intervals.
         Args:
             message (String): The plaintext message that is send to the receiver.
             receiver (String): The IP-address of the receiver.
         """
-
         # Prepare both the raw ping message and a socket.
         packet = Ether() / IP(dst=IPs.dst, src=IPs.src) / ICMP()
         frame = bytearray(raw(packet))
         s = conf.L2socket()
         # Send pings at message intervals.
-        intervals = self.message_converter.message_to_intervals(message, base)
+        intervals = self.message_converter.message_to_intervals(
+            message, base, delay)
         s.send(frame)
         for interval in intervals:
             time.sleep(interval)
@@ -73,8 +90,21 @@ class Sender(object):
 
 
 if __name__ == "__main__":
-    smc = SimpleMessageConverter()
-    simple_sender = Sender(smc)
-    message = "test"
-    intervals = simple_sender.send_message(message, 7)
-    print(intervals)  # Print for debugging.
+    args = parser()
+
+    # Initialize sender
+    if args.type == 1:
+        mc = SimpleMessageConverter()
+    elif args.type == 2:
+        mc = NoIntervalMessageConverter()
+    else:
+        raise NotImplementedError
+
+    # Send message
+    simple_sender = Sender(mc)
+    message = args.msg
+    intervals = simple_sender.send_message(message, args.base, args.delay)
+
+    # Print info
+    print(f"Message: {message}\n" +
+          f"Ping at intervals (ms): {intervals * 1000}")
